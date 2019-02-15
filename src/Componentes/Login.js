f485feae55a3be5cac6545eb4e6a8d88eff4ab94
carlos114771 <carlosalfredoriveramilian@yahoo.es>
2019-02-15 16:47:26 -0600

    navigation bar

---
+++ src/Componentes/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button,Form, FormGroup,Label,Input,FormText} from 'reactstrap';
+
+export default class LoginForm extends React.Component{
+    render(){
+        return(
+            <Form>
+                <FormGroup>
+                    
+                </FormGroup>
+            </Form>
+        );
+    }
+}
